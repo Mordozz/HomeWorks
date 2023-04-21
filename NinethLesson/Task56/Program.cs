4ef087dd7fcbfc99009a1850ab418bc37e1b1373
Anton Balashov <tyquat@gmail.com>
2023-04-21 18:19:24 +0300

    Homework 56 redone

--- NinethLesson/Task56/Program.cs
+++ NinethLesson/Task56/Program.cs
@@ -63,10 +63,10 @@ int lowestSumInRow(int[,] array)
     int minSum = int.MaxValue;
     int minSumRow = -1;
     int rowSum = 0;
-    for (int i = 0; i < array.GetLength(1); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
         rowSum = 0;
-        for (int j = 0; j < array.GetLength(0); j++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
             rowSum += array[i, j];
         }
