0946254d8279fb3012d3944472d85ba98cefe851
Anton Balashov <tyquat@gmail.com>
2023-04-27 19:48:07 +0300

    Homework 64,66,68 done

---
+++ TenthLesson/Task64/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+int InputInterface(string message)
+{
+    Console.Write(message);
+    string input = Console.ReadLine();
+    int value = Convert.ToInt32(input);
+    return value;
+}
+
+void RecursionDecrease(int n)
+{
+    Console.Write(n);
+    if (n > 1)
+    {
+        Console.Write(", ");
+        RecursionDecrease(n - 1);
+    }
+}
+
+
+int n = InputInterface("Введите значение N: ");
+RecursionDecrease(n);
---
+++ TenthLesson/Task68/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+
+int InputInterface(string message)
+{
+    Console.Write(message);
+    string input = Console.ReadLine();
+    int value = Convert.ToInt32(input);
+    return value;
+}
+
+int Ackerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (n == 0)
+        return Ackerman(m - 1, 1);
+    else
+        return Ackerman(m - 1, Ackerman(m, n - 1));
+}
+
+int m = InputInterface("Задайте значение M: ");
+int n = InputInterface("Задайте значение N: ");
+int result = Ackerman(m, n);
+Console.WriteLine($"A({m}, {n}) = {result}");
